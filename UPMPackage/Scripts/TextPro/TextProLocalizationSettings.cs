978d4ddc930a49866770938d2d2884cdde89ae5b
Sadegh <73638842+sadegh-askari@users.noreply.github.com>
2025-01-22 18:21:46 +0330

    update upm package

--- UPMPackage/Scripts/TextPro/TextProLocalizationSettings.cs
+++ UPMPackage/Scripts/TextPro/TextProLocalizationSettings.cs
@@ -1,20 +1,25 @@
 #if UNITY_EDITOR
 using UnityEditor.Localization;
 #endif
+using System;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Localization.Settings;
+
 namespace YoYo.UI
 {
-    [CreateAssetMenu(menuName = "Localization/TextPro LocalizationSettings", fileName = "TextProLocalizationSettings")]
-    public class TextProLocalizationSettings : ScriptableObject
+    [CreateAssetMenu(menuName = "Localization/TextPro LocalizationCollection", fileName = "TextProLocalizationCollection")]
+    public class TextProLocalizationCollection : ScriptableObject
     {
-        [SerializeField] private int _counter;
+        [SerializeField] private LocalizationUser _activeUser;
+        [SerializeField] private List<TextProLocalizationSettings> _localizationSettings;
 
         [SerializeField] private bool _overrideLocalize;
 
-        #if UNITY_EDITOR
+#if UNITY_EDITOR
         [SerializeField] private StringTableCollection _stringTable;
         public StringTableCollection StringTable => _stringTable;
-        #endif
+#endif
 
         [SerializeField] private string[] _locales;
         public bool OverrideLocalization => _overrideLocalize;
@@ -22,7 +27,43 @@ namespace YoYo.UI
 
         public int GetLocalizeKeyIndex()
         {
+            var settings = GetActiveSettings();
+            if (settings == null)
+            {
+                throw new NullReferenceException("Cannot find Localization setting for active user");
+            }
+
+            return settings.GetLocalizeKeyIndex();
+        }
+
+        private TextProLocalizationSettings GetActiveSettings()
+        {
+            if (_localizationSettings == null || (int)_activeUser == 0) return null;
+            return _localizationSettings.Find(settings => settings.user == _activeUser);
+        }
+    }
+    
+    [CreateAssetMenu(menuName = "Localization/TextPro LocalizationSettings", fileName = "TextProLocalizationSettings")]
+    public class TextProLocalizationSettings : ScriptableObject
+    {
+        public LocalizationUser user; 
+        [SerializeField] private int _counter;
+
+        public int GetLocalizeKeyIndex()
+        {
+            if (_counter == 0)
+                _counter = (int)user;
+            
             return _counter++;
         }
+    }
+    
+    public enum LocalizationUser
+    {
+        Sadegh = 1,
+        Parsa = 1000000,
+        Akbar = 2000000,
+        Kourosh = 3000000,
+        Sina = 4000000,
     }
 }
